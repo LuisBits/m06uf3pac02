3bd66ea5593847f5bf999848994a49f373eca514
madlevel <yosiul@me.com>
2016-12-10 21:03:49 +0100

    Add files via upload

--- codigoJS.js
+++ codigoJS.js
@@ -47,9 +47,9 @@ var Funciones = {
     // Si la conexión es correcta, muestro mensaje y ejecuto listado, según la página en que se encuentre.
     onsuccess: function (evento) {
         console.log("Conexión a BBDD correcta");
+        console.log(window.location.pathname);
         // Si nos encontramos en la página de listar o modificar, mostramos la tabla o fila.
         if (window.location.pathname == '/html/listaCentros.html' || window.location.pathname == '/html/modificarCentros.html') {
-             console.log(window.location.pathname);
             Funciones.listarCentros();
         }
         if (window.location.pathname == '/html/actualizarCentros.html') {
